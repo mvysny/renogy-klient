c4b13c028933e0e3ad6b5e9af9d25b144606707d
Martin Vysny <mavi@vaadin.com>
2023-04-14 08:47:15 +0300

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
+
+plugins {
+    kotlin("jvm") version "1.8.20"
+    application
+}
+
+defaultTasks("clean", "build")
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("com.github.mvysny.vokorm:vok-orm:2.0")
+    implementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.slf4j:slf4j-simple:2.0.6")
+
+    // database drivers
+    implementation("org.postgresql:postgresql:42.5.1")
+
+    // tests
+    testImplementation("com.github.mvysny.dynatest:dynatest:0.24")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        // to see the exceptions of failed tests in Travis-CI console.
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
