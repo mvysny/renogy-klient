e16d434616715ab2b1cac20d8d3a9c71aada2928
Martin Vysny <mavi@vaadin.com>
2023-04-15 13:31:52 +0300

    Removed unused stuff

--- src/main/kotlin/utils/TimeUtils.kt
+++ src/main/kotlin/utils/TimeUtils.kt
@@ -1,61 +1,7 @@
 package utils
 
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 import java.time.Instant
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.ZonedDateTime
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
 operator fun Instant.minus(other: Instant): Duration = (this.toEpochMilli() - other.toEpochMilli()).milliseconds
-
-object LocalTimeSerializer : KSerializer<LocalTime> {
-    override val descriptor: SerialDescriptor
-        get() = PrimitiveSerialDescriptor("LocalTime", PrimitiveKind.STRING)
-
-    override fun deserialize(decoder: Decoder): LocalTime = LocalTime.parse(decoder.decodeString())
-
-    override fun serialize(encoder: Encoder, value: LocalTime) {
-        encoder.encodeString(value.toString())
-    }
-}
-
-object LocalDateSerializer : KSerializer<LocalDate> {
-    override val descriptor: SerialDescriptor
-        get() = PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
-
-    override fun deserialize(decoder: Decoder): LocalDate = LocalDate.parse(decoder.decodeString())
-
-    override fun serialize(encoder: Encoder, value: LocalDate) {
-        encoder.encodeString(value.toString())
-    }
-}
-
-object LocalDateTimeSerializer : KSerializer<LocalDateTime> {
-    override val descriptor: SerialDescriptor
-        get() = PrimitiveSerialDescriptor("LocalDateTime", PrimitiveKind.STRING)
-
-    override fun deserialize(decoder: Decoder): LocalDateTime = LocalDateTime.parse(decoder.decodeString())
-
-    override fun serialize(encoder: Encoder, value: LocalDateTime) {
-        encoder.encodeString(value.toString())
-    }
-}
-
-object ZonedDateTimeSerializer : KSerializer<ZonedDateTime> {
-    override val descriptor: SerialDescriptor
-        get() = PrimitiveSerialDescriptor("ZonedDateTime", PrimitiveKind.STRING)
-
-    override fun deserialize(decoder: Decoder): ZonedDateTime = ZonedDateTime.parse(decoder.decodeString())
-
-    override fun serialize(encoder: Encoder, value: ZonedDateTime) {
-        encoder.encodeString(value.toString())
-    }
-}
