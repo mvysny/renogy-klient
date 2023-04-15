33057eaee68a9e178ec8aeac30808ba158c8aafc
Martin Vysny <mavi@vaadin.com>
2023-04-15 13:33:11 +0300

    minor

--- src/main/kotlin/utils/TimeUtils.kt
+++
@@ -1,7 +0,0 @@
-package utils
-
-import java.time.Instant
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
-
-operator fun Instant.minus(other: Instant): Duration = (this.toEpochMilli() - other.toEpochMilli()).milliseconds
--- src/main/kotlin/utils/Utils.kt
+++ src/main/kotlin/utils/Utils.kt
@@ -1,7 +1,11 @@
 package utils
 
+import java.io.PrintStream
+import java.time.Instant
 import kotlin.random.Random
 import kotlin.random.nextUInt
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 fun Random.nextFloat(from: Float, to: Float): Float =
     nextDouble(from.toDouble(), to.toDouble()).toFloat()
@@ -23,3 +27,21 @@ fun Byte.toHex(): String = toUByte().toInt().toString(16).padStart(2, '0')
  * Converts every byte in the array to char and concatenates it as a string. Pure ASCII is used, no UTF-8 conversion is done.
  */
 fun ByteArray.toAsciiString() = toString(Charsets.US_ASCII)
+
+operator fun Instant.minus(other: Instant): Duration = (this.toEpochMilli() - other.toEpochMilli()).milliseconds
+
+/**
+ * Simple CSV writer, writing CSV lines to given [out].
+ */
+class CSVWriter(private val out: PrintStream) {
+    fun writeLine(vararg line: Any?) {
+        val row = line.joinToString(",") {
+            when (it) {
+                null -> ""
+                is Number, is UShort, is UInt, is UByte -> it.toString()
+                else -> "\"$it\""
+            }
+        }
+        out.println(row)
+    }
+}
