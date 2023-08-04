1183b60b3628e37e2c3f03f907b605c69c0503e3
Martin Vysny <mavi@vaadin.com>
2023-08-04 12:11:32 +0300

    #4 influx tests: delete old measurements

--- src/test/kotlin/InfluxDBDataLoggerTest.kt
+++ src/test/kotlin/InfluxDBDataLoggerTest.kt
@@ -1,21 +1,30 @@
 import clients.dummyRenogyData
 import com.github.mvysny.dynatest.DynaNodeGroup
 import com.github.mvysny.dynatest.DynaTestDsl
+import com.influxdb.client.InfluxDBClient
+import com.influxdb.client.InfluxDBClientFactory
 import org.testcontainers.containers.InfluxDBContainer
 import org.testcontainers.utility.DockerImageName
+import java.time.OffsetDateTime
 
 @DynaTestDsl
 fun DynaNodeGroup.influxDBDataLoggerTests() {
     group("InfluxDB2DataLogger") {
         val token = "tgPiZMSv30US40AX_v9zV-dTexHeJ1u4zNCQYEGNW13DNbLiCUFxpVLPZZtX7C0f8UfN84oS3jRxaKWlAICKmA=="
         lateinit var container: InfluxDBContainer<*>
+        lateinit var client: InfluxDBClient
         beforeGroup {
             container = InfluxDBContainer(DockerImageName.parse("influxdb:2.7.1"))
             container.withAdminToken(token)
             container.start()
         }
+        beforeGroup {
+            client = InfluxDBClientFactory.create(container.url, token.toCharArray(), container.organization, container.bucket)
+        }
+        afterGroup { client.close() }
         afterGroup { container.stop() }
-        // @todo delete the data between the tests
+        beforeEach { client.deleteApi.delete(OffsetDateTime.now().minusYears(1), OffsetDateTime.now().plusYears(1), "", container.bucket, container.organization) }
+
         test("smoke") {
             InfluxDB2Logger(container.url, container.organization, container.bucket, token).use {
                 it.init()
