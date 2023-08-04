e8cfde9a8d557bfa80911f2d6e58e624abd14d0c
Martin Vysny <mavi@vaadin.com>
2023-08-04 12:03:36 +0300

    minor

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
 tasks.withType<Test> {
     useJUnitPlatform()
     testLogging {
-        // to see the exceptions of failed tests in Travis-CI console.
+        // to see the exceptions of failed tests in the CI console.
         exceptionFormat = TestExceptionFormat.FULL
     }
 }
