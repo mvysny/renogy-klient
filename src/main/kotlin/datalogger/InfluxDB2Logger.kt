b41affec7d4873885582698bbfae3ee152d6d86b
Martin Vysny <mavi@vaadin.com>
2023-10-14 13:00:50 +0300

    #8 parse InfluxDB2 error when it fails

--- src/main/kotlin/datalogger/InfluxDB2Logger.kt
+++ src/main/kotlin/datalogger/InfluxDB2Logger.kt
@@ -2,9 +2,12 @@ package datalogger
 
 import clients.RenogyData
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import utils.Log
+import java.lang.RuntimeException
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -83,7 +86,10 @@ class InfluxDB2Logger(val url: String, val org: String, val bucket: String, val 
         val response: HttpResponse<String> = client.send(request,
             HttpResponse.BodyHandlers.ofString()
         )
-        check(response.statusCode() in 200..299) { "InfluxDB2 failed: ${response.statusCode()} ${response.body()}" }
+        if (response.statusCode() !in 200..299) {
+            val failure = InfluxDBFailure.parse(response)
+            throw InfluxDBException(content, failure)
+        }
     }
 
     override fun deleteRecordsOlderThan(days: Int) {
@@ -120,3 +126,42 @@ data class InfluxDBDeleteRequest(
     val stop: String,
     val predicate: String? = null
 )
+
+/**
+ * @property httpErrorCode e.g. 500 for internal server error
+ */
+data class InfluxDBFailure(
+    val httpErrorCode: Int,
+    val error: InfluxDBError
+) {
+    companion object {
+        private val log = Log<InfluxDBFailure>()
+
+        fun parse(response: HttpResponse<String>): InfluxDBFailure {
+            val statusCode = response.statusCode()
+            val json = response.body()
+            val err = try {
+                Json.decodeFromString<InfluxDBError>(json)
+            } catch (e: SerializationException) {
+                log.debug("Failed to deserialize ${json}: $e", e)
+                InfluxDBError("unknown", json)
+            }
+            return InfluxDBFailure(statusCode, err)
+        }
+    }
+}
+
+/**
+ * @property code for example "internal error"
+ * @property message unexpected error writing points to database: timeout
+ */
+@Serializable
+data class InfluxDBError(
+    val code: String,
+    val message: String,
+)
+
+class InfluxDBException(
+    val requestBody: String,
+    val failure: InfluxDBFailure
+) : RuntimeException("Failed to POST $requestBody: $failure")
